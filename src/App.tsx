69a400bbd4046e4f294ce1da7d513127675ed1d7
Dzmitry <dzmi3y@gmail.com>
2024-03-11 22:19:57 +0300

    add main files and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { GalleryPage } from './pages/GalleryPage';
+import { OrderPage } from './pages/OrderPage';
+import { ExhibitionsPage } from './pages/ExhibitionsPage';
+import { TamaraDementjevaPage } from './pages/TamaraDementjevaPage';
+import { PageNotFound } from './pages/PageNotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Sh_art
-        </p>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" Component={HomePage} />
+        <Route path="/gallery" Component={GalleryPage} />
+        <Route path="/order" Component={OrderPage} />
+        <Route path="/exhibitions" Component={ExhibitionsPage} />
+        <Route path="/tamara_dementjeva" Component={TamaraDementjevaPage} />
+        <Route Component={PageNotFound} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/TamaraDementjevaPage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const TamaraDementjevaPage = () => {
+  return (
+    <div>TamaraDementjevaPage</div>
+  )
+}

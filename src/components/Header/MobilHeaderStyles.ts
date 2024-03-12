6bc8a7552a1d3ae83e9b383963c5301a6e65fdd3
Dzmitry <dzmi3y@gmail.com>
2024-03-12 23:04:03 +0300

    clean files

--- src/components/Header/MobilHeaderStyles.ts
+++ src/components/Header/MobilHeaderStyles.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { HashLink } from 'react-router-hash-link';
 
 export const Container = styled.div`
     display:flex;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { DesktopHeader } from './DesktopHeader'
 import { MobilHeader } from './MobilHeader'
-import styled from 'styled-components'
-
-
 
 export const Header = () => {
     return (<>

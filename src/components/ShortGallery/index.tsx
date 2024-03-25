72ead7b879268b6187c70e72a43d82df6d2276cb
Dzmitry <dzmi3y@gmail.com>
2024-03-25 12:43:20 +0300

    add slider to short galery in home page

--- src/components/ShortGallery/index.tsx
+++ src/components/ShortGallery/index.tsx
@@ -7,7 +7,27 @@ import { CardContainer, Container, ImgCard, PaginationContainer, PaginationDot }
 export const ShortGallery: React.FC<{ invert?: boolean, imgList: [string, string, string] }> = ({ imgList, invert }) => {
   const [dotsClasses, setDotsClasses] = useState<[string, string, string]>(["current", "next", "previous"]);
   const [previousSelectedCard, setPreviousSelectedCard] = useState<number>(0);
+  const [touchStart, setTouchStart] = useState(null);
+  const [touchEnd, setTouchEnd] = useState(null);
 
+  const minSwipeDistance = 50
+
+  const onTouchStart = (e:any) => {
+    setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+    setTouchStart(e.targetTouches[0].clientX)
+  }
+
+  const onTouchMove = (e:any) => setTouchEnd(e.targetTouches[0].clientX)
+
+  const onTouchEnd = () => {
+    if (!touchStart || !touchEnd) return
+    const distance = touchStart - touchEnd
+    const isLeftSwipe = distance > minSwipeDistance
+    const isRightSwipe = distance < -minSwipeDistance
+    if (isLeftSwipe || isRightSwipe) {
+       isLeftSwipe ? nextCard() :  previousCard();
+    }
+  }
 
   const nextCard = () => {
     const next: number = (previousSelectedCard + 1) % 3;
@@ -24,7 +44,7 @@ export const ShortGallery: React.FC<{ invert?: boolean, imgList: [string, string
     const next: number = (cardNumber + 1) % 3;
     const previous: number = (cardNumber + 2) % 3;
 
-    const rotationAnimation = previousSelectedCard == next ? "left" : "right";
+    const rotationAnimation = previousSelectedCard == next ? "rightAnimation" : "leftAnimation";
 
     let newClasses: [string, string, string] = ["", "", ""];
     newClasses[cardNumber] = "current " + rotationAnimation;
@@ -36,9 +56,9 @@ export const ShortGallery: React.FC<{ invert?: boolean, imgList: [string, string
   }
 
   return (
-    <Container>
+    <Container onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd}>
       <CardContainer className={invert ? "invert" : ""}>
-        {imgList.map((imgSrc, i) => (<ImgCard key={"card_" + i} src={imgSrc} className={dotsClasses[i]}/>))}
+        {imgList.map((imgSrc, i) => (<ImgCard key={"card_" + i} src={imgSrc} className={dotsClasses[i]} />))}
       </CardContainer>
       <PaginationContainer>
         {imgList.map((_, i) => (<PaginationDot key={"card_" + i} onClick={() => { dotClick(i) }} className={dotsClasses[i]} />))}
--- src/components/ShortGallery/styles.ts
+++ src/components/ShortGallery/styles.ts
@@ -1,87 +1,175 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    height: 370px;
+    @media (min-width: 1458px) {
+        height: 611px;
+    }
+`;
 export const CardContainer = styled.div`
     display: flex;
     justify-content: center;
+    height: 320px;
+    margin-top: 20px;
 
     @media (min-width: 1458px) {
+        height: 1000px;
+        
         img:nth-child(2){
-            margin-left: -40px;
-            margin-top: 20px;
+            margin-left: -40px !important;
+            margin-top: 20px !important;
         }
         img:nth-child(3){
-            margin-left: -40px;
-            margin-top: 40px;
+            margin-left: -40px !important;
+            margin-top: 40px !important;
         }
-
 
         &.invert{
             img:nth-child(1){
-                margin-top: 40px;
+                margin-top: 40px !important;
             }
             img:nth-child(2){
-                margin-left: -40px;
-                margin-top: 20px;
+                margin-left: -40px !important;
+                margin-top: 20px !important;
             }
             img:nth-child(3){
-                margin-left: -40px;
-                margin-top: 0px;
+                margin-left: -40px !important;
+                margin-top: 0px !important;
             }
         }
     }
 `;
 export const ImgCard = styled.img`
 
-    &.next{
-        display: none;
-    }
+    @media (max-width: 1457px) {
+        @keyframes previousLeftAnimation {
+                0%   {width: 283px; height: 280px;margin-top: 49px; position: absolute; margin-left: 0px; z-index: 2;}
+                100% {width: 227px; height: 219px;margin-top: 0px; position: absolute; margin-left: -90px; z-index: 1;}
+        }
+        @keyframes currentLeftAnimation {
+                0%   {width: 183px; height: 180px;margin-top: 200px; position: absolute; margin-left: 100px; z-index: 3; opacity: 0%}
+                100% {width: 283px; height: 280px;margin-top: 49px; position: absolute; margin-left: 0px; z-index: 2; opacity: 100%}
+        }
+        @keyframes nextLeftAnimation {
+                0%   {width: 227px; height: 219px;margin-top: 0px; position: absolute; margin-left: -90px; z-index: 1; opacity: 100%}
+                100% {width: 227px; height: 219px;margin-top: 0px; position: absolute; margin-left: 10px; z-index: 0;opacity: 0%}
+        }
 
-    &.current{
-        width: 283px;
-        height: 280px;
-        max-width: 100%;
-        max-height: 100%;
-        margin-top: 49px;
-        z-index: 1;
-    }
 
-    &.previous{
-        width: 227px;
-        height: 219px;
-        max-width: 100%;
-        max-height: 100%;
-        position: absolute;
-        margin-left: -90px;
-        z-index: 0;
-    }
+        @keyframes  currentRightAnimation{
+                100%   {width: 283px; height: 280px;margin-top: 49px; position: absolute; margin-left: 0px; z-index: 2;}
+                0%      {width: 227px; height: 219px;margin-top: 0px; position: absolute; margin-left: -90px; z-index: 1;}
+        }
+        @keyframes   nextRightAnimation{
+                100%   {width: 183px; height: 180px;margin-top: 200px; position: absolute; margin-left: 100px; z-index: 3; opacity: 0%}
+                0%      {width: 283px; height: 280px;margin-top: 49px; position: absolute; margin-left: 0px; z-index: 2; opacity: 100%}
+        }
+        @keyframes previousRightAnimation {
+                100%   {width: 227px; height: 219px;margin-top: 0px; position: absolute; margin-left: -90px; z-index: 1; opacity: 100%}
+                0% {width: 227px; height: 219px;margin-top: 0px; position: absolute; margin-left: 10px; z-index: 0;opacity: 0%}
+        }
 
+
+
+        &.next{
+            opacity: 0%;
+            &.leftAnimation{
+                
+                animation: 1s ease-in-out normal nextLeftAnimation;
+                animation-fill-mode: forwards;
+            }
+            &.rightAnimation{
+                
+                animation: 1s ease-in-out normal nextRightAnimation;
+                animation-fill-mode: forwards;
+            }
+        }
+
+        &.current{
+            max-width:  283px;
+            max-height: 280px;
+            width: 100%;
+            height: 100%;
+            margin-top: 49px;
+            position: absolute;
+            z-index: 1; 
+            &.leftAnimation{
+                
+                animation: 1s ease-in-out normal currentLeftAnimation;
+                animation-fill-mode: forwards;
+            }
+            &.rightAnimation{
+                
+                animation: 1s ease-in-out normal currentRightAnimation;
+                animation-fill-mode: forwards;
+            }
+
+        }
+
+        &.previous{
+            width: 100%;
+            height: 100%;
+            max-width: 227px ;
+            max-height: 219px ;
+            position: absolute;
+            margin-left: -90px;
+            z-index: 0; 
+            
+            &.leftAnimation{    
+                //start position
+                max-width:  283px;
+                max-height: 280px;
+                width: 100%;
+                height: 100%;
+                margin-top: 49px;
+                position: absolute;
+                z-index: 1; 
+                
+                animation: 1s ease-in-out normal previousLeftAnimation;
+                animation-fill-mode: forwards;
+            }
+            &.rightAnimation{
+                
+                animation: 1s ease-in-out normal previousRightAnimation;
+                animation-fill-mode: forwards;
+            }
+        }
+    }
 
     @media (min-width: 1458px) {
-        max-width: 556px;
-        max-height: 550px;
-        width: 100%;
-        height: 100%;
-        box-shadow: var(--shadow);
+        &.leftAnimation,
+        &.rightAnimation,
+        &.next,
+        &.previous,
+        &.current{
+            position: static;
+            max-width: 556px;
+            max-height: 550px;
+            width: 100%;
+            height: auto;
+        }
     }
 `;
+
 export const PaginationContainer = styled.div`
     display:flex;
     justify-content: center;
     gap: 9px;
     margin-top: 34px;
-    
+    position: absolute;
+    z-index: 4;
+    width: 100%;
+    margin-left: -2rem;
     @media (min-width: 1458px) {
         display: none;
     }
-
 `;
+
 export const PaginationDot = styled.div`
     cursor: pointer;
     border-radius: 50%;
     width: 16px;
     height: 16px;
-
     
     &.next{
         background-color: #626D8E;

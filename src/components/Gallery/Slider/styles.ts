318c50a27c2b952d197fab7f03c1a285e9186f66
Dzmitry <dzmi3y@gmail.com>
2024-04-01 22:49:57 +0300

    add mobil markup to slider

--- src/components/Gallery/Slider/styles.ts
+++ src/components/Gallery/Slider/styles.ts
@@ -18,93 +18,139 @@ export const Container = styled.div`
 `;
 
 export const Modal = styled.div`
-    background-color: var(--modal-bg-color);
-    position: fixed;
-    top:130px;
-    left: 160px;
-    right: 160px;
-    height: calc(100vh - 160px);
-    z-index: 2;
-    padding: 0 20px;
-    display: grid;
-    grid-template-rows: 110px calc(100vh - 440px) 170px;
+        background-color: var(--modal-bg-color);
+        position: fixed;
+        top: 60px;
+        left: 0px;
+        right: 0px;
+        height: 100vh ;
+        z-index: 2;
+        //padding: 0 20px;
+        display: grid;
+        grid-template-rows: 59px calc(100vh - 320px) 201px;
+
+    @media (min-width: 1458px) {
+        top:130px;
+        left: 160px;
+        right: 160px;
+        height: calc(100vh - 160px);
+
+        padding: 0 20px;
+        display: grid;
+        grid-template-rows: 110px calc(100vh - 440px) 170px;
+    }
 `;
 
 export const HeadContainer = styled.div`
     margin-top: 9px;
     display: grid;
-    grid-template-columns: 1fr 2fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    @media (min-width: 1458px) {
+        margin-top: 9px;
+        grid-template-columns: 1fr 2fr 1fr;
+    }
 `;
 export const ControlsContainer = styled.div`
-    width: 277px;
-    display: flex;
-    margin-top: 17px;
-    gap: 43px;
+    width: 16px;
+    @media (min-width: 1458px) {
+        width: 277px;
+        display: flex;
+        margin-top: 17px;
+        gap: 43px;
+    }
 `;
 export const Minus = styled.img`
-    cursor: pointer;
-    height: 58px;
-    width: 58px;
-    content: url(/images/slider/light/minus.png);
-    &:hover{
-        content: url(/images/slider/light/hover/minus.png);
-    }
-    &:active{
-        content: url(/images/slider/light/active/minus.png);
+    display: none;
+    @media (min-width: 1458px) {
+        display: block;
+        cursor: pointer;
+        height: 58px;
+        width: 58px;
+        content: url(/images/slider/light/minus.png);
+        &:hover{
+            content: url(/images/slider/light/hover/minus.png);
+        }
+        &:active{
+            content: url(/images/slider/light/active/minus.png);
+        }
     }
 `;
 export const Plus = styled.img`
-    cursor: pointer;
-    height: 58px;
-    width: 58px;
-    content: url(/images/slider/light/plus.png);
-    &:hover{
-        content: url(/images/slider/light/hover/plus.png);
-    }
-    &:active{
-        content: url(/images/slider/light/active/plus.png);
+    display: none;
+    @media (min-width: 1458px) {
+        display: block;
+        cursor: pointer;
+        height: 58px;
+        width: 58px;
+        content: url(/images/slider/light/plus.png);
+        &:hover{
+            content: url(/images/slider/light/hover/plus.png);
+        }
+        &:active{
+            content: url(/images/slider/light/active/plus.png);
+        }
     }
 `;
 export const Play = styled.img`
-    cursor: pointer;
-    height: 51px;
-    width: 30px;
-    margin-top: 7px;
-    margin-left: 20px;
+    height: 22px;
+    width: 17px;
+    // margin-top: 7px;
+    // margin-left: 20px;
     content: url(/images/slider/light/play.png);
-    &:hover{
-        content: url(/images/slider/light/hover/play.png);
-    }
     &:active{
         content: url(/images/slider/light/active/play.png);
     }
+
+    @media (min-width: 1458px) {
+        cursor: pointer;
+        height: 51px;
+        width: 30px;
+        margin-top: 7px;
+        margin-left: 20px;
+        &:hover{
+            content: url(/images/slider/light/hover/play.png);
+        }
+       
+    }
 `;
 export const Stop = styled.img`
-    margin-top: 7px;
-    margin-left: 12px;
-    height: 50px;
-    width: 38px;
+    //margin-top: 7px;
+    //margin-left: 12px;
+    height: 21px;
+    width: 16px;
     cursor: pointer;
     content: url(/images/slider/light/stop.png);
-    &:hover{
-        content: url(/images/slider/light/hover/stop.png);
-    }
     &:active{
         content: url(/images/slider/light/active/stop.png);
     }
+    @media (min-width: 1458px) {
+        margin-top: 7px;
+        margin-left: 12px;
+        height: 50px;
+        width: 38px;
+        cursor: pointer;
+        &:hover{
+            content: url(/images/slider/light/hover/stop.png);
+        }
+    }
 `;
 export const Close = styled.img`
-    margin-top: 11px;
     justify-self: end;
-    height: 35px;
-    width: 35px;
-    cursor: pointer;
+    height: 16px;
+    width: 16px;
     content: url(/images/slider/light/close.png);
-    &:hover{
-        content: url(/images/slider/light/hover/close.png);
-    }
     &:active{
         content: url(/images/slider/light/active/close.png);
+    }
+    @media (min-width: 1458px) {
+        margin-top: 11px;
+        justify-self: end;
+        height: 35px;
+        width: 35px;
+        cursor: pointer;
+        &:hover{
+            content: url(/images/slider/light/hover/close.png);
+        }
     }
 `;
 
@@ -116,100 +162,195 @@ export const TitleContainer = styled.div`
 `;
 
 export const Title = styled.div`
+    display: none;
+    @media (min-width: 1458px) {
+        display: block;
+        text-align: center;
+        font-family: Playfair;
+        font-size: 40px;
+        color: var(--title-color);
+    }
+`;
+export const MobilTitle = styled.div`
+    display: block;
     text-align: center;
     font-family: Playfair;
-    font-size: 40px;
+    font-size: 24px;
     color: var(--title-color);
+
+    @media (min-width: 1458px) {
+        display: none;
+    }
 `;
 export const Description = styled.div`
     text-align: center;
     font-family: Varta;
-    font-size: 24px;
+    font-size: 15px;
     color: var(--description-color);
+    @media (min-width: 1458px) {
+        font-size: 24px;
+    }
 `;
 
 export const MainContainer = styled.div`
     display: grid;
-    grid-template-columns: 140px calc(100vw - 640px) 140px;
+    grid-template-columns: 2rem calc(100vw - 4rem) 2rem;
+   // width: 100px; //////////////////
+    @media (min-width: 1458px) {
+        grid-template-columns: 140px calc(100vw - 640px) 140px;
+    }
 `;
 
 export const BackgroundImageContainer = styled.div`
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
     border-radius: 4px;
+    background-image: none !important;;
+    @media (min-width: 1458px) {
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+    }
 `;
 export const DarkEffectDiv = styled.div`
-    background: rgba(0, 0, 0, 0.7 );
+    @media (min-width: 1458px) {
+        background: rgba(0, 0, 0, 0.7 );
+    }
     width: 100%;
     height: 100%;
 `;
 export const ImageContainer = styled.div`
     display: flex;
-    height: calc(100vh - 460px);
-    width: calc(100vw - 640px);
     overflow: hidden;
     justify-content: center;
     align-items: center;
-    padding-top: 10px;
-    padding-bottom: 10px;
+    /* padding-top: 10px;
+    padding-bottom: 10px; */
+    height: 100%;
+    width: calc(100vw - 4rem);
+    @media (min-width: 1458px) {
+        
+        height: calc(100vh - 460px);
+        width: calc(100vw - 640px);
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
 `;
 export const Image = styled.img`
-    
-    height: 100%;
+    width: 100%;
+    @media (min-width: 1458px) {
+        height: 100%;
+        width: auto;
+    }
 `;
 
 export const LeftImgControlContainer = styled.div`
     align-self: center;
-    width: 43px;
+    width: 53px;
+    @media (min-width: 1458px) {
+        width: 43px;
+    }
 `;
 
 export const LeftImgControl = styled.img`
     transform: rotate(180deg);
-    width: 43px;
-    height: 86px;
+    width: 53px;
+    height: 53px;
+    border-radius: 50%;
+    background: rgba(255,255,255, 0.5);
     cursor: pointer;
-    content: url(/images/slider/light/vertical_angle_arrow.png);
-    &:hover{
-        content: url(/images/slider/light/hover/vertical_angle_arrow.png);
-    }
+    content: url(/images/slider/light/mobil_vertical_angle_arrow.png);
     &:active{
-        content: url(/images/slider/light/active/vertical_angle_arrow.png);
+        content: url(/images/slider/light/active/mobil_vertical_angle_arrow.png);
+    }
+    margin-left: 35px;
+    z-index: 1;
+    position: absolute;
+    @media (min-width: 1458px) {
+        background: none;
+        border-radius: 0%;
+        position: static;
+        margin-left: 0px;
+        width: 43px;
+        height: 86px;
+        cursor: pointer;
+        content: url(/images/slider/light/vertical_angle_arrow.png);
+        &:hover{
+            content: url(/images/slider/light/hover/vertical_angle_arrow.png);
+        }
+        &:active{
+            content: url(/images/slider/light/active/vertical_angle_arrow.png);
+        }
     }
 `;
 
 
 export const RightImgControlContainer = styled.div`
-    width: 43px;
+    width: 53px;
     justify-self: end;
     align-self: center;
+    @media (min-width: 1458px) {
+        width: 43px;
+    }
 `;
 
 export const RightImgControl = styled.img`
-    width: 43px;
-    height: 86px;
+    width: 53px;
+    height: 53px;
     justify-self: end;
     align-self: center;
     cursor: pointer;
-    content: url(/images/slider/light/vertical_angle_arrow.png);
-    &:hover{
-        content: url(/images/slider/light/hover/vertical_angle_arrow.png);
-    }
+    content: url(/images/slider/light/mobil_vertical_angle_arrow.png);
     &:active{
-        content: url(/images/slider/light/active/vertical_angle_arrow.png);
+        content: url(/images/slider/light/active/mobil_vertical_angle_arrow.png);
+    }
+    margin-left: -35px;
+    z-index: 1;
+    position: absolute;
+    border-radius: 50%;
+    background: rgba(255,255,255, 0.5);
+    @media (min-width: 1458px) {
+        background: none;
+        border-radius: 0%;
+        position: static;
+        margin-left: 0px;
+        width: 43px;
+        height: 86px;
+        justify-self: end;
+        align-self: center;
+        cursor: pointer;
+        content: url(/images/slider/light/vertical_angle_arrow.png);
+        &:hover{
+            content: url(/images/slider/light/hover/vertical_angle_arrow.png);
+        }
+        &:active{
+            content: url(/images/slider/light/active/vertical_angle_arrow.png);
+        }
     }
 `;
 
 
 export const BottomContainer = styled.div`
     display: flex;
-    justify-content: space-between;
-    margin: 0 140px;
+    flex-direction: column;
+    justify-content: center;
+    @media (min-width: 1458px) {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 0 140px;
+       /// width: 100px;///////////////
+    }
 `;
+
 export const Price = styled.div`
-    font-size: 96px;
+    font-size: 40px;
     font-family: Playfair;
     color: var(--price-color);
+    text-align: center;
+    @media (min-width: 1458px) {
+        text-align: start;
+        font-size: 40px; 
+    }
+    
 `;
 export const ButtonContainer = styled.div`
 `;
